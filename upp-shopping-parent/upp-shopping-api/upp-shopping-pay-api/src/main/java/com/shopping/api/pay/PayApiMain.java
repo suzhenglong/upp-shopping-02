0181f8c33878cd9f3eb9d5bb4192627995f64ccd
suzl <zhenglongsu@163.com>
2018-12-12 10:02:54 +0800

    PayApiMain

---
+++ upp-shopping-parent/upp-shopping-api/upp-shopping-pay-api/src/main/java/com/shopping/api/pay/PayApiMain.java
@@ -0,0 +1,19 @@
+package com.shopping.api.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @author: zhenglongsu@163.com
+ * @date: 2018.09.29 14:54
+ */
+@SpringBootApplication
+public class PayApiMain {
+    public static void main(String[] args) {
+        SpringApplication.run(PayApiMain.class, args);
+    }
+}
+
+
+
